d387bca21f90e8449ad91ce8cea6bbfe6c33043d
Neon Minuit <contact@neonminuit.xyz>
2021-06-13 18:58:39 +0200

    center

--- ply.js
+++ ply.js
@@ -3,11 +3,11 @@ window.onload = function() {
 
 	var request = new XMLHttpRequest();
 	request.open("GET", "ply/boulette.ply", true);
-	request.responseType = "arraybuffer";
+	// request.responseType = "arraybuffer";
 	request.onload = function(e) {
 		var index = request.responseURL.indexOf('data');
 		var letter = request.responseURL.substr(index).split('/')[1];
-
+		console.log(request.response)
 		var bitReader = new BitReader(request.response);
 		var lineText = "";
 		var vertexCount = 0;
@@ -16,19 +16,33 @@ window.onload = function() {
 		var normals = [];
 		var colors = [];
 		var header = true;
-		var index = 0;
-		while (bitReader.byteIndex < bitReader.uint8Array.length) {
+		var offset = 0;
+		var scale = 0.1;
+		// property float x
+		// property float y
+		// property float z
+		// property float nx
+		// property float ny
+		// property float nz
+		// property uchar red
+		// property uchar green
+		// property uchar blue
+		// property uchar alpha
+		var stripe = 3*32+3*32+4*8;
+		while (offset < request.response.length) {
+		// while (bitReader.byteIndex < bitReader.uint8Array.length) {
 			if (header) {
-				var v = String.fromCharCode(bitReader.read(8));
+				var v = request.response[offset];//String.fromCharCode(bitReader.read(8));
 				if (v == '\n') {
 					if (lineText.includes("end_header")) {
 						header = false;
+						console.log(lineText)
 					} else if (lineText.includes("element vertex")) {
 						var array = lineText.split(' ');
 						if (array.length > 0) {
 							var subtractor = array.length - 2;
 							vertexCount = (array [array.length - subtractor]);
-							var view = new DataView(new ArrayBuffer(vertexCount));
+							var view = new DataView(new ArrayBuffer(vertexCount*(3+3+4)));
 						}
 					} else if (lineText.includes("property uchar alpha")) {
 						colorDataCount = 4;
@@ -38,27 +52,56 @@ window.onload = function() {
 					lineText += v;
 				}
 			} else {
-				// view.setUint32(bitReader.read(32), bitReader.read(32), bitReader.read(32));
-				// view.setUint32(bitReader.read(32), bitReader.read(32), bitReader.read(32));
-				// view.setUint8(bitReader.read(8), bitReader.read(8), bitReader.read(8));
-				// positions.push(bitReader.read(32), bitReader.read(32), bitReader.read(32));
-				// normals.push(bitReader.read(32), bitReader.read(32), bitReader.read(32));
-				// colors.push(bitReader.read(8)/255, bitReader.read(8)/255, bitReader.read(8)/255);
-				if (colorDataCount == 4) {
-					bitReader.read(8);
-					view.push(bitReader.read(8));
+				var v = request.response[offset];//String.fromCharCode(bitReader.read(8));
+				if (v == '\n') {
+					const datas = lineText.split(' ');
+					positions.push(datas[0]*scale, datas[1]*scale, datas[2]*scale);
+					normals.push(datas[3], datas[4], datas[5]);
+					colors.push(datas[6], datas[7], datas[8], 1);
+					lineText = "";
+				} else {
+					lineText += v;
 				}
+				// view.setFloat32(offset++, bitReader.read(32), true);
+				// view.setFloat32(offset++, bitReader.read(32), true);
+				// view.setFloat32(offset++, bitReader.read(32), true);
+				// view.setFloat32(offset++, bitReader.read(32), true);
+				// view.setFloat32(offset++, bitReader.read(32), true);
+				// view.setFloat32(offset++, bitReader.read(32), true);
+				// view.setUint8(offset++, bitReader.read(8), true);
+				// view.setUint8(offset++, bitReader.read(8), true);
+				// view.setUint8(offset++, bitReader.read(8), true);
+				// // positions.push(bitReader.read(32), bitReader.read(32), bitReader.read(32));
+				// // normals.push(bitReader.read(32), bitReader.read(32), bitReader.read(32));
+				// // colors.push(bitReader.read(8)/255, bitReader.read(8)/255, bitReader.read(8)/255);
+				// // if (colorDataCount == 4) {
+				// 	view.setUint8(offset++, bitReader.read(8), true);
+				// 	// view.setUint8(bitReader.read(8));
+				// 	// view.push(bitReader.read(8));
+				// 	;
+				// // }
 			}
+			++offset;
 		}
-		console.log(view.getFloat32(0));
-
+		// console.log(vertexCount);
+		
+		// for (var i = 0; i < vertexCount; ++i)
+		// {
+		// 	var ii = i;
+		// 	positions.push(view.getFloat32(ii, true), view.getFloat32(ii+1, true), view.getFloat32(ii+2, true));
+		// 	normals.push(view.getFloat32(ii+3, true), view.getFloat32(ii+4, true), view.getFloat32(ii+5, true));
+		// 	colors.push(view.getFloat32(ii+6, true), view.getFloat32(ii+7, true), view.getFloat32(ii+8, true), view.getFloat32(ii+9, true));
+		// }
+		console.log(positions[0]);
+		
 		const canvas = document.getElementById("canvas");
 		const gl = canvas.getContext("webgl");
 		const programInfo = twgl.createProgramInfo(gl, ["vs", "fs"]);
-		const geometry = { position:positions };
-		// const count = 10000;
+		// const geometry = { position:[] };
+		const geometry = { position:positions, normal: normals, color: colors };
+		const count = 10000;
 		// for (var v = 0; v < count; ++v) {
-		// 	geometry.position.push(Math.random(),Math.random(),Math.random());
+		// 	geometry.position.push(Math.random()*2-1,Math.random()*2-1,Math.random()*2-1);
 		// }
 		const bufferInfo = twgl.createBufferInfoFromArrays(gl, geometry);
 		var cameraDistance = 5;
